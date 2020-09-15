0d7801e29f6dd0909d723d1a0a2d2f2de1001a97
euloong <euloong@hotmail.com>
2020-09-15 13:46:40 +1000

    updated shop data with unique identifiers, added cart utils to group items in cart reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import CartActionTypes from './cart.types';
+import {addItemsToCart} from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -15,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
-                cartItems: [...state.cartItems, action.payload]
+                cartItems: addItemsToCart(state.cartItems, action.payload)
             };    
         default:
             return state;
