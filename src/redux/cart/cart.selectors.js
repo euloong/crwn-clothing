415b3e2537864d67d344ab91044515553a5c83d6
euloong <euloong@hotmail.com>
2020-09-21 13:04:10 +1000

    added createStructuredSelector from reselect to pass in props for mapStateToProps

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,6 +7,11 @@ export const selectCartItems = createSelector(
     cart => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
+);
+
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => cartItems.reduce((totalQuantity, cartItem) =>  totalQuantity + cartItem.quantity, 0)
