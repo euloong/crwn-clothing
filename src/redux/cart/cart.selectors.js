898b7346ace7b9a51db65ef10dae40c15f45f982
euloong <euloong@hotmail.com>
2020-09-21 19:54:19 +1000

    added checkout page component, headers and total calculation

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,3 +16,9 @@ export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => cartItems.reduce((totalQuantity, cartItem) =>  totalQuantity + cartItem.quantity, 0)
 );
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce((totalQuantity, cartItem) =>  
+    totalQuantity + cartItem.quantity * cartItem.price, 0)
+);
