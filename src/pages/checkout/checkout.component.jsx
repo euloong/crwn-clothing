6555851f1782f73ec1c47208cace5685655ed28a
euloong <euloong@hotmail.com>
2020-09-22 13:51:42 +1000

    added checkout item component and updated checkout page to show cart items

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
@@ -25,10 +27,10 @@ const CheckoutPage = ({cartItems, total}) => (
                 <span>Remove</span>
             </div>
         </div> 
-            {cartItems.map(cartItem => cartItem.name)}  
-            <div className='total'>
-                <span>TOTAL: ${total}</span>
-            </div> 
+        {cartItems.map(cartItem => (
+            <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))}  
+        <div className='total'>TOTAL: ${total}</div> 
     </div>
 );
 
